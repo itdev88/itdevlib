4bc068fbe4b354a134908b9eb3432bde9c9037b7
itdev88 <itdev88sby@gmail.com>
2022-11-30 06:34:56 +0700

    itdevlib

--- itdev88/src/main/java/com/ahmadveb/itdev88/base/BaseActivity.kt
+++ itdev88/src/main/java/com/ahmadveb/itdev88/base/BaseActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AlertDialog
 import com.ahmadveb.itdev88.R
-\import com.ahmadveb.itdev88.ui.ext.toast
+import com.ahmadveb.itdev88.ui.ext.toast
 import com.ahmadveb.itdev88.utils.ExtensionFunctions.toast
 import com.ahmadveb.itdev88.utils.Helper
 
